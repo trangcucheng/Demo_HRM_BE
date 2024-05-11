d50dc53ca68ba2eace2e9777e0998372f1dae9b2
trangcucheng <trangcucheng@gmail.com>
2024-05-12 00:31:38 +0700

    update https

--- src/main.ts
+++ src/main.ts
@@ -17,15 +17,16 @@ async function bootstrap() {
     /* SSl config */
     const keyPath = './secrets/privkey.pem'; // /etc/letsencrypt/live/[domain]/privkey.pem
     const certPath = './secrets/fullchain.pem'; // /etc/letsencrypt/live/[domain]/fullchain.pem
-    let httpsOptions;
-    if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
-        httpsOptions = {
-            key: fs.readFileSync(keyPath),
-            cert: fs.readFileSync(certPath),
-        };
-    }
+    // let httpsOptions;
+    // if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
+    //     httpsOptions = {
+    //         key: fs.readFileSync(keyPath),
+    //         cert: fs.readFileSync(certPath),
+    //     };
+    // }
 
-    const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions });
+    // const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions });
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     /* app config */
     app.enableVersioning({ type: VersioningType.URI });
